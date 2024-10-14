8e9f6bdc6311523ea3427c9b00e8974ec77df4c1
albertusm <abmeiring@gmail.com>
2024-10-14 23:39:48 +0200

    first commit

---
+++ src/main/java/com/albertus/timely/timely/User.java
@@ -0,0 +1,24 @@
+package com.albertus.timely.timely;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String role;
+}
